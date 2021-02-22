d1f8c5a0fe1df3f9781be1e71e14374262d6b1b8
Ethan Huynh <e2huynh@ucsd.edu>
2021-02-21 16:51:22 -0800

    Largely finished header

---
+++ tests/components/header.test.js
@@ -0,0 +1,31 @@
+'use strict';
+// we use fs to read the html file to load it up
+const fs = require('fs');
+test('test main.js', () => {
+    // we create a fake testing document here:
+    document.body.innerHTML = `<div>
+        <pomo-header></pomo-header>
+    </div>`
+
+    // we use require here to load the component javascript
+    require("../../source/components/header/header.js")
+
+    // find our component elements
+    const header = document.getElementsByName("pomo-header");
+    const histBtn = header.getElementById("hist");
+    const helpBtn = header.getElementById("help");
+
+    //TODO:check logo is loaded in
+    
+
+    //TODO:check links work
+
+
+    // test that the text content between the nav buttons is correct
+    expect(histBtn.innerText).toBe("History");
+    expect(helpBtn.innerText).toBe("Help");
+
+    // verify the correct classes are applied
+    expect(histBtn.elements.button.className).toBe("pomo-button nav");
+    expect(helpBtn.elements.button.className).toBe("pomo-button nav");
+});
