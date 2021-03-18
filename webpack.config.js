de6da09ecef9ccde1bd09f29eee7a411b84f3447
Stone Tao <stonezt2019@gmail.com>
2021-03-18 16:10:54 -0700

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'assets',
+    publicPath: './',
     clean: true
   },
   module: {
