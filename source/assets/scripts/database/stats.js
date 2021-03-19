86c8a93df3e5613dd602021cc71738d65ce595ea
Stone Tao <stonezt2019@gmail.com>
2021-03-18 21:06:35 -0700

    fix bugs

--- source/assets/scripts/database/stats.js
+++ source/assets/scripts/database/stats.js
@@ -12,7 +12,7 @@ export class PomoStats {
 
         let id = getLatestSessionID();
         let counter = 0;
-        while(id!= 0) {
+        while(id != -1) {
             let session_object_date = new Date(getObject(POMO_SESSION_MAP)[id]["startDate"]);
             if(this.date.getMonth() === session_object_date.getMonth() && this.date.getDate() === session_object_date.getDate() && this.date.getYear() === session_object_date.getYear()){
                 Object.keys(getObject(POMO_SESSION_MAP)[id]["allTasks"])
@@ -21,7 +21,7 @@ export class PomoStats {
             else if(this.date.getMonth() > session_object_date.getMonth() || this.date.getDate() > session_object_date.getDate() || this.date.getYear() > session_object_date.getYear()){
                 break;
             } 
-            id-=1;
+            id -= 1;
         }
         for(let taskID in this.tasks) {
             if(this.tasks[taskID].completed) {
